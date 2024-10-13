9c099828b982cfad20191bea30fbf26da2bd1801
Jason <jlee55504@gmail.com>
2024-10-12 21:05:57 -0700

    Added and imported 'middleware functions' 'asyncErrorBoundary', 'hasProperties', and 'reservationExists'. The 'asyncErrorBoundary' function checks for any 'async errors'. The 'hasProperties' function checks has the correct 'properties' or throws the proper error to users. the 'reservationExists' function checks checks if the requested 'restaurant reservation' exists in the database. Also included are the 'route handlers' 'read', 'list', and 'create'. The 'read' 'route handler' retrieves the specified 'restaurant reservation(s)' for users. The 'list' 'route handler' displays all the specified 'restaurnt reservations' for users. The 'create' 'route handler' cretate a new 'restaurant reservation' in the database. The 'route handlers' are 'exported' using the proper 'middleware functions'.

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -6,16 +6,6 @@ const reservationsService = require("./reservations.service");
 const hasProperties = require("../errors/hasProperties");
 const { Resolver } = require("webpack");
 
-
-const VALID_PROPERTIES = [
-  "first_name",
-  "last_name",
-  "mobile_number",
-  "reservation_date",
-  "reservation_time",
-  "people",
-];
-
 // Middleware functions
 
 const hasRequiredProperties = hasProperties(
@@ -25,55 +15,38 @@ const hasRequiredProperties = hasProperties(
   "reservation_date",
   "reservation_time",
   "people");
-const hasOnlyValidProperties = (req, res, next) => {
-  const { data = {} } = req.body;
-  const invalidFields = Object.keys(data).filter((field) => !VALID_PROPERTIES.includes(field));
-  if (invalidFields.length) {
-    return next({
-      status: 400,
-      message: `Invalid field(s): ${invalidFields.join(", ")}`,
-    });
-  };
-  next();
-};
+
 
 const reservationExists = async (req, res, next) => {
- // const {data: {reservation_time} = {}} = req.body
-
   const reservation = await reservationsService.read(req.params.date);
-  //console.log(req.params.date)
   if (reservation) {
     res.locals.reservation_date = reservation;
     return next();
-  }
+  };
   next({
     status: 404,
-    message: "Reservation cannot be found."
+    message: "Reservation cannot be found.",
   });
-}
+};
 
 // Route handlers
 const read = (req, res) => {
-  //const data = { reservation: data } = res.locals;
  const data = res.locals.reservation_date;
-  res.json({ data })
-}
+  res.json({ data });
+};
 
 async function list(req, res) {
   const data = await reservationsService.list();
-  res.json({
-    data
-  });
-}
+  res.json({ data });
+};
 
 const create = async(req, res) => {
-
   const data = await reservationsService.create(req.body.data);
   res.status(201).json({ data });
-}
+};
 
 module.exports = {
   read: [asyncErrorBoundary(reservationExists), read],
   list: [asyncErrorBoundary(list)],
-  create: [hasOnlyValidProperties, hasRequiredProperties, asyncErrorBoundary(create)],
+  create: [hasRequiredProperties, asyncErrorBoundary(create)],
 };
