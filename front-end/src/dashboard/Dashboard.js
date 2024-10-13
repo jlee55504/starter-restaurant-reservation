5295508e887b81eeed6fc1a0427461d65b6bdf05
Jason <jlee55504@gmail.com>
2024-10-13 13:49:26 -0700

    Added 'React Button elements' and code to display the 'restaurant reservations' for 'today's' date, the next 'day's' date, and the previous 'day's' date.

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -32,19 +32,19 @@ function Dashboard({ date }) {
           if (reservations.length > 0) {
             reservationsWithDates = reservations.filter(reservation => reservation.reservation_date === queryParams)
             .map(reservation => { 
-              if (reservation.reservation_date === queryParams){ 
+              if (reservation.reservation_date === queryParams) { 
                 return reservation.reservation_date;
-              }
+              };
             });        
-          } if (reservationsWithDates.length <= 0) {  
+          }; if (reservationsWithDates.length <= 0) {  
               setReservationsWithDate([]);
               return [];
           } else {
               return readReservation(reservationsWithDates, abortController.signal);
-            }        
+            };        
         } else {
             return Promise.reject(reservations);
-          }
+          };
       })
       .then(setReservationsWithDate)
       .catch((response) => {
@@ -54,14 +54,14 @@ function Dashboard({ date }) {
           .map(reservation => { 
             if (reservation.reservation_date === date) { 
               return reservation.reservation_date;
-            }
+            };
           }) 
-        } if (todaysReservations.length <= 0) {
+        }; if (todaysReservations.length <= 0) {
             setReservations([]);
             return [];
           } else {
               return setReservations(todaysReservations);
-            }
+            };
       })
       .catch(setReservationsError);
       return () => abortController.abort();
@@ -77,19 +77,17 @@ function Dashboard({ date }) {
       { reservationsWithDate.length > 0 ?
        JSON.stringify(reservationsWithDate) : null }
       { reservations.length > 0 && reservationsWithDate.length === 0 ? JSON.stringify(reservations) : null }
-      { reservations.length === 0 && reservationsWithDate.length === 0 ? <h6>No reservations today</h6> : null }
+      { reservations.length === 0 && reservationsWithDate.length === 0 ? <h6>No reservations for this day</h6> : null }
       <div>
           <Button className="btn btn-secondary" onClick={() => {
-            history.push(`/dashboard?date=${previous(date)}`)
+            history.push(`/dashboard?date=${previous(date)}`);
             }}>Previous day</Button>
           <Button className="btn btn-primary" onClick={() =>{ 
-            history.push(`/dashboard?date=${today()}`)
-            }
-            }>Today</Button>
+            history.push(`/dashboard?date=${today()}`);
+            }}>Today</Button>
           <Button className="btn btn-secondary" onClick={() =>{ 
-            history.push(`/dashboard?date=${next(date)}`)
-            }
-            }>Next day</Button>
+            history.push(`/dashboard?date=${next(date)}`);
+            }}>Next day</Button>
       </div>
     </main>
   );
