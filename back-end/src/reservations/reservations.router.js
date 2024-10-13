fb558d1adbb5f57b1291c80e977a1861e2198910
Jason <jlee55504@gmail.com>
2024-10-12 21:34:46 -0700

    Unnecessary code removed and code spaced-out.

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -7,14 +7,12 @@
 const router = require("express").Router();
 const controller = require("./reservations.controller");
 const methodNotAllowed =  require("../errors/methodNotAllowed");
-const cors = require("cors");
 
 router.route("/")
-.all(cors())
-//.get(controller.read)
     .get(controller.list)
     .post(controller.create)
     .all(methodNotAllowed);
 router.route("/:date")
     .get(controller.read)
+    .all(methodNotAllowed);
 module.exports = router;
