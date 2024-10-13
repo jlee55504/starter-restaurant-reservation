493591c88f24880587f9ee3053d4a469a0b2f827
Jason <jlee55504@gmail.com>
2024-10-12 21:42:32 -0700

    Added 'date' 'parameters' for the 'Dshboard' 'compenents' for the '/dashboard/:reservationDate' and '/dashboard' 'routes'.

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -5,6 +5,7 @@ import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
 import CreateNewReservation from "../reservations/CreateNewReservation";
+import useQuery from '../utils/useQuery';
 /**
  * Defines all the routes for the application.
  *
@@ -13,6 +14,7 @@ import CreateNewReservation from "../reservations/CreateNewReservation";
  * @returns {JSX.Element}
  */
 function Routes() {
+  const query = useQuery();
   return (
     <Switch>
       <Route exact={true} path="/">
@@ -26,10 +28,10 @@ function Routes() {
         <CreateNewReservation />
       </Route>
       <Route path="/dashboard/:reservationDate">
-        <Dashboard date={today()} />
+      <Dashboard date={query.get("date")} />
       </Route>
       <Route path="/dashboard">
-        <Dashboard date={today()} />
+        <Dashboard date={query.get("date") || today()} />
       </Route>
       <Route>
         <NotFound />
