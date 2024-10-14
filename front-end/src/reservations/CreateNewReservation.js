19b33aafab2c7c66b1567de4d4b4be2211eb13b0
Jason <jlee55504@gmail.com>
2024-10-13 20:08:46 -0700

    Added code which checks that the new 'restaurant reservation's' date isn't on Tuesdays and isn't a date in the past. Added starter code to check for the correct time.

--- front-end/src/reservations/CreateNewReservation.js
+++ front-end/src/reservations/CreateNewReservation.js
@@ -7,44 +7,73 @@ import { Button } from "react-bootstrap";
 import {readReservation} from '../utils/api';
 import ErrorAlert from "../layout/ErrorAlert";
 import {makeNewReservation }from '../utils/api';
-import { formatAsDate } from "../utils/date-time";
+import { formatAsDate, formatAsTime } from "../utils/date-time";
   
 
 function CreateNewReservation() {
     const history = useHistory();
-    const [ first_name, set_first_name ] = useState("");
-    const [ last_name, set_last_name ] = useState("");
-    const [mobile_number, set_mobile_number] = useState("");
-    const [reservation_date, set_reservation_date] = useState("");
-    const [error, set_error] = useState(null)
-    const [reservation_time, set_reservation_time] = useState("");
-    const [people, set_people] = useState(1);
+    const [ firstName, setFirstName ] = useState("");
+    const [ lastName, setLastName ] = useState("");
+    const [mobileNumber, setMobileNumber] = useState("");
+    const [reservationDate, setReservationDate] = useState("");
+    const [error, setError] = useState(null)
+    const [reservationTime, setReservationTime] = useState("");
+    const [people, setPeople] = useState(1);
+    const [apiErrors, setApiErrors] = useState(null);
+    let errorsArray = [];
     const handleChange = ({ target }) => {
-        if (target.name === "first_name") set_first_name( target.value );
-        if (target.name === "last_name") set_last_name(target.value);
-        if (target.name === "mobile_number") set_mobile_number(target.value);
-        if (target.name === "reservation_date") set_reservation_date(target.value);
-        if (target.name === "reservation_time") set_reservation_time(target.value);
-        if (target.name === "people") set_people(target.value);
+        if (target.name === "first_name") setFirstName( target.value );
+        if (target.name === "last_name") setLastName(target.value);
+        if (target.name === "mobile_number") setMobileNumber(target.value);
+        if (target.name === "reservation_date") setReservationDate(target.value);
+        if (target.name === "reservation_time") setReservationTime(target.value);
+        if (target.name === "people") setPeople(target.value);
     }
 
     const handleSubmit = event => {
         event.preventDefault();
         const abortController = new AbortController();
         const newReservation = {
-            first_name: first_name,
-            last_name: last_name,
-            mobile_number: mobile_number,
-            reservation_date: reservation_date,
-            reservation_time: reservation_time,
+            first_name: firstName,
+            last_name: lastName,
+            mobile_number: mobileNumber,
+            reservation_date: reservationDate,
+            reservation_time: reservationTime,
             people: Number(people),
         };
+        let newReservationDate = new Date(newReservation.reservation_date.replace(/-/g, '\/'));
+        let reservationDateToCompare = parseInt(newReservation.reservation_date.replace(/-/g, '\/').split('/').join(''));
+        let todaysDate = new Date();
+        console.log("reservation_time", newReservation.reservation_time)
+        //console.log("todaysDate: ",todaysDate.toLocaleTimeString('eo', { hour12: false }))
+        const todaysTime = `${todaysDate.getHours()}:${todaysDate.getMinutes()}`
+        console.log(todaysTime)
+        //console.log(todaysDate.getMinutes())
+        todaysDate = parseInt(todaysDate.toISOString().slice(0,10).replace(/-/g,""));
         makeNewReservation(newReservation, abortController.signal)    
         .then((newReservation) => {
-            history.push(
+                history.push(
               `/dashboard?date=${formatAsDate(newReservation.reservation_date)}`
-            )})
-            .catch(set_error);
+            );
+        })
+        .catch(apiErrors);
+
+        if (reservationDateToCompare < todaysDate && newReservationDate.getDay() !== 2) {
+            setError(new Error("Only future reservations are allowed. "));
+        } if (newReservationDate.getDay() === 2 && reservationDateToCompare > todaysDate) {
+            setError(new Error("This restaurant is closed on Tuesdays. Please try again. "));
+          } //if ()
+           else if (reservationDateToCompare < todaysDate && newReservationDate.getDay() === 2) {
+                errorsArray.push(new Error("This restaurant is closed on Tuesdays. Please try again. "));
+                errorsArray.push(new Error("Only future reservations are allowed."));
+                setError(errorsArray);
+            }
+        setFirstName("");
+        setLastName("");
+        setMobileNumber("");
+        setReservationDate("");
+        setReservationTime("");
+        setPeople(1);
     }
     return (
       <main>
@@ -53,19 +82,19 @@ function CreateNewReservation() {
             <form onSubmit={handleSubmit}>
               <div>
                 <label htmlFor="first_name">
-                    <input type="text" name="first_name" id="first_name" placeholder="First Name" value={ first_name } onChange={ handleChange } maxLength={ 50 } required></input>
+                    <input type="text" name="first_name" id="first_name" placeholder="First Name" value={ firstName } onChange={ handleChange } maxLength={ 50 } required></input>
                 </label>
                 <label htmlFor="last_name">
-                    <input type="text" name="last_name" id="last_name" placeholder="Last Name" value={ last_name } onChange={ handleChange } maxLength={ 50 } required></input>
+                    <input type="text" name="last_name" id="last_name" placeholder="Last Name" value={ lastName } onChange={ handleChange } maxLength={ 50 } required></input>
                 </label>
                 <label htmlFor="mobile_number">
-                    <input type="text" name="mobile_number" id="mobile_number" placeholder="Enter a mobile phone number" value={ mobile_number } onChange={ handleChange }  required/>
+                    <input type="text" name="mobile_number" id="mobile_number" placeholder="Enter a mobile phone number" value={ mobileNumber } onChange={ handleChange }  required/>
                 </label>
                 <label htmlFor="reservation_date">
-                    <input type="date" name="reservation_date" id="reservation_date" placeholder="YYYY-MM-DD" pattern="\d{4}-\d{2}-\d{2}" value={ reservation_date } onChange={ handleChange } required/>
+                    <input type="date" name="reservation_date" id="reservation_date" placeholder="YYYY-MM-DD" pattern="\d{4}-\d{2}-\d{2}" value={ reservationDate } onChange={ handleChange } required/>
                 </label>
                 <label htmlFor="reservation_time">
-                    <input type="time" name="reservation_time" id="reservation_time" placeholder="HH:MM" pattern="[0-9]{2}:[0-9]{2}" value={ reservation_time } onChange={ handleChange } required/>
+                    <input type="time" name="reservation_time" id="reservation_time" placeholder="HH:MM" pattern="[0-9]{2}:[0-9]{2}" value={ reservationTime } onChange={ handleChange } required/>
                 </label>
                 <label htmlFor="people">
                     <input type="number" name="people" id="people" placeholder="Number of people" pattern="[0-9]*" value={ people } onChange={ handleChange } min={ 1 }  required></input>
