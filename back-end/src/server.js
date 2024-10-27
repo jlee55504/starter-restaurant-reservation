dd7e9ee652a2037859bb279574ef7836adba0896
Jason <jlee55504@gmail.com>
2024-10-26 21:49:37 -0700

    Adding code to get the project running on render.com.

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 5000;
+const { PORT = 5000 } = process.env;
 
 const app = require("./app");
 const knex = require("./db/connection");
@@ -7,7 +7,7 @@ knex.migrate
   .latest()
   .then((migrations) => {
     console.log("migrations", migrations);
-    app.listen(port, listener);
+    app.listen(PORT, listener);
   })
   .catch((error) => {
     console.error(error);
@@ -15,5 +15,5 @@ knex.migrate
   });
 
 function listener() {
-  console.log(`Listening on Port ${port}!`);
+  console.log(`Listening on Port ${PORT}!`);
 }
